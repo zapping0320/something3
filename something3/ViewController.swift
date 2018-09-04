db7b9ce43a99146558b64d649c913feb24c92437
John DongHyun Kim <zapping0320@gmail.com>
2018-09-05 07:25:41 +0900

    add notebook view controller

--- something3/ViewController.swift
+++ something3/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
+     @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableview.delegate = self
+        self.tableview.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +27,46 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+        /*
+        if (section > 1 || section < 0){
+            return 0
+        }else{
+            let datalist = notebookarray[section] as [R_Notebook]!
+            return datalist!.count
+        }*/
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30.0
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        /*
+        let viewController = self.storyboard?.instantiateViewController(withIdentifier: "NotebookContent View") as! NotebookContentViewController
+        let currentNoteBook = notebookarray[indexPath.section]![indexPath.row] as R_Notebook
+        viewController.selectedNotebook = currentNoteBook
+        self.navigationController?.pushViewController(viewController, animated: true)
+        //self.present(viewController, animated: true)
+ */
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        /*
+        let cell:NotebookTableViewCell = self.tableview.dequeueReusableCell(withIdentifier: "notebook cell", for: indexPath) as! NotebookTableViewCell
+        let currentitem = notebookarray[indexPath.section]![indexPath.row] as R_Notebook
+        //print(indexPath.row)
+        //print(currentitem.name)
+        cell.label_ol_name?.text = currentitem.name
+        
+        return cell
+ */
+    }
+    
+}
