b5c4c77b36413ecfcfde0f251c290c807bab3128
John DongHyun Kim <zapping0320@gmail.com>
2018-09-15 05:08:04 +0900

    1 implement deleting notebook

--- something3/ViewControllers/NotebookViewController.swift
+++ something3/ViewControllers/NotebookViewController.swift
@@ -139,5 +139,49 @@ extension NotebookViewController {
         let selectedNotebook = notebookArray_[index.section]![index.row] as R_NoteBook
         contentsVC.selectedNoteBookId = selectedNotebook.id
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        let currentNotebook = notebookArray_[indexPath.section]![indexPath.row] as R_NoteBook
+        if(currentNotebook.id != -1)
+        {
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            let alert = UIAlertController(title: title,
+                                          message: "Are you sure want to delete this notebook?",
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            
+            let yesAction = UIAlertAction(title: "YES",
+                                          style: .default, handler:
+                { action in
+                    let currentNotebook = self.notebookArray_[indexPath.section]![indexPath.row] as R_NoteBook
+                    let realm = try! Realm()
+                    try! realm.write {
+                        
+                        let predicate = NSPredicate(format: "relatedNotebookId = %@",  NSNumber(value: currentNotebook.id))
+                        for note in realm.objects(R_Note.self).filter(predicate){
+                            note.relatedNotebookId = -1
+                            note.oldNotebookId = currentNotebook.id
+                        }
+                        realm.delete(currentNotebook)
+                    }
+                    self.loadNotebooks()
+                }
+            )
+            
+            let cancelAction = UIAlertAction(title: "No",
+                                             style: .cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            alert.addAction(yesAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 
 }
