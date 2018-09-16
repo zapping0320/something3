7a2aa826dc2954c6e964cd3c2b8343ce9e257377
John DongHyun Kim <zapping0320@gmail.com>
2018-09-17 07:28:25 +0900

    1 favorite

--- something3/ViewControllers/FavoriteViewController.swift
+++ something3/ViewControllers/FavoriteViewController.swift
@@ -36,15 +36,22 @@ class FavoriteViewController: UIViewController, UITableViewDelegate, UITableView
         self.tableview?.reloadData()
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        guard let noteVC : NoteViewController = segue.destination as? NoteViewController
+            else {
+                return
+        }
+        
+        guard let cell:UITableViewCell = sender as? UITableViewCell else {
+            return
+        }
+        
+        guard  let index:IndexPath = self.tableview?.indexPath(for: cell)  else {
+            return
+        }
+        
+        noteVC.selectedNote = favoriteNotes[index.row]
     }
-    */
 
 }
 
