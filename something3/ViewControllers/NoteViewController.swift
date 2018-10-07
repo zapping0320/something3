289cbc48777085329fd1f48962bba12af5486e93
John DongHyun Kim <zapping0320@gmail.com>
2018-10-07 09:42:46 +0900

    implement alarm set @NoteViewVC

--- something3/ViewControllers/NoteViewController.swift
+++ something3/ViewControllers/NoteViewController.swift
@@ -15,6 +15,7 @@ class NoteViewController: UIViewController,UITextViewDelegate,UIPickerViewDataSo
     @IBOutlet weak var tf_title: UITextField!
     @IBOutlet weak var tv_content: UITextView!
     @IBOutlet weak var switch_favorite: UISwitch!
+    @IBOutlet weak var switch_alarm: UISwitch!
     @IBOutlet weak var pv_notebooks: UIPickerView!
     
     @IBOutlet weak var bt_more: UIButton!
@@ -23,12 +24,15 @@ class NoteViewController: UIViewController,UITextViewDelegate,UIPickerViewDataSo
     open var selectedNote:R_Note = R_Note()
     fileprivate var notebookArray_ = [R_NoteBook]()
     let dateformatter = DateFormatter()
+    var alarmDate:Date?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dateformatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
         self.lb_updatedAt.text = dateformatter.string(from: selectedNote.updated_at)
         self.switch_favorite.isOn = selectedNote.isfavorite
+        self.switch_alarm.isOn = selectedNote.alarmDate != nil ? true : false
+        self.alarmDate = selectedNote.alarmDate
         
         self.tf_title.text = selectedNote.title
         self.tf_title.placeholder = "Title"
@@ -46,11 +50,13 @@ class NoteViewController: UIViewController,UITextViewDelegate,UIPickerViewDataSo
             self.bt_more.isHidden = false
             self.lb_guideTrash.isHidden = false
             self.switch_favorite.isHidden = true
+            self.switch_alarm.isHidden = true
         }
         else{
             self.bt_more.isHidden = true
             self.lb_guideTrash.isHidden = true
             self.switch_favorite.isHidden = false
+            self.switch_alarm.isHidden = false
             self.pv_notebooks.isAccessibilityElement = true
         }
          loadNotebooks()
@@ -192,7 +198,7 @@ class NoteViewController: UIViewController,UITextViewDelegate,UIPickerViewDataSo
     
     
     @IBAction func lb_title_ValueChanged(_ sender: Any) {
-        saveChangedData()
+         saveChangedData()
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -220,6 +226,7 @@ class NoteViewController: UIViewController,UITextViewDelegate,UIPickerViewDataSo
             selectedNote.isfavorite = self.switch_favorite.isOn
             selectedNote.relatedNotebookId = notebookArray_[pv_notebooks.selectedRow(inComponent: 0)].id
             selectedNote.updated_at = Date()
+            selectedNote.alarmDate = self.alarmDate
         }
         
         self.lb_updatedAt.text = dateformatter.string(from: selectedNote.updated_at)
@@ -227,6 +234,46 @@ class NoteViewController: UIViewController,UITextViewDelegate,UIPickerViewDataSo
     }
     
     @IBAction func switch_ValueChanged(_ sender: UISwitch) {
-        saveChangedData()
+        if(sender.tag == 1){
+            saveChangedData()
+        }
+        else if(sender.tag == 2)
+        {
+            let datePicker = UIDatePicker()
+            datePicker.datePickerMode = .dateAndTime
+            if(self.selectedNote.alarmDate != nil) {
+                datePicker.setDate(self.selectedNote.alarmDate!, animated: false)
+            }
+            
+            let alert = UIAlertController(title: "\n\n\n\n\n\n\n\n\n\n\nAlarm Setting", message: nil, preferredStyle: .actionSheet)
+            alert.view.addSubview(datePicker)
+            /*
+             datePicker.snp.makeConstraints { (make) in
+             make.centerX.equalTo(alert.view)
+             make.top.equalTo(alert.view).offset(8)
+             }*/
+            
+            let setAction = UIAlertAction(title: "설정", style: .default) { (action) in
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd hh:mm"
+                let dateString = dateFormatter.string(from: datePicker.date)
+                print(dateString)
+                self.alarmDate = datePicker.date
+                self.saveChangedData()
+            }
+            
+            let unsetAction = UIAlertAction(title: "설정해제", style: .default) { (action) in
+                self.alarmDate = nil
+                self.saveChangedData()
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+            
+            alert.addAction(setAction)
+            alert.addAction(unsetAction)
+            alert.addAction(cancel)
+            
+            present(alert, animated: true, completion: nil)
+        }
     }
 }
