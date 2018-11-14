cd669c1353bf8293e9be7a164239bee986221621
John DongHyun Kim <zapping0320@gmail.com>
2018-11-15 07:51:50 +0900

    Setting ViewController 추가

---
+++ something3/ViewControllers/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  something3
+//
+//  Created by 김동현 on 15/11/2018.
+//  Copyright © 2018 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
