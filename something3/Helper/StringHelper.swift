62c33aca3cd0293311961f9a7c1eca3e9f2a78b9
John DongHyun Kim <zapping0320@gmail.com>
2018-11-15 07:22:51 +0900

    1.StringHelper class 추가 2.copied subtitle을 static 함수로 처리하게 함

---
+++ something3/Helper/StringHelper.swift
@@ -0,0 +1,15 @@
+//
+//  StringHelper.swift
+//  something3
+//
+//  Created by 김동현 on 15/11/2018.
+//  Copyright © 2018 John Kim. All rights reserved.
+//
+
+import Foundation
+
+class StringHelper {
+    static func makeHeaderStringCopied(title : String) -> String {
+        return title + " Copied"
+    }
+}
