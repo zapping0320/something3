9834222108f0398293d0bdc9e93192d3a5b594df
John DongHyun Kim <zapping0320@gmail.com>
2018-11-24 11:22:19 +0900

    UserDefault를 이용해서 다크모드 구현
    [todo]
    - 다크모드에서의 메인/서브 버튼색/ 텍스트 색 정해서 적용하기

--- something3/Helper/ColorHelper.swift
+++ something3/Helper/ColorHelper.swift
@@ -13,10 +13,21 @@ class ColorHelper {
     static let mainBackgroundColor = UIColor(red: 255/255, green: 175/255, blue: 50/255, alpha: 1)
     
     static func getCurrentAppBackground() -> UIColor {
-        return mainBackgroundColor
+        let defaults = UserDefaults.standard
+        let darkmode = defaults.bool(forKey: "darkMode")
+        if(darkmode == true)
+        {
+            return UIColor.lightGray
+        }
+        else
+        {
+            return mainBackgroundColor
+        }
     }
     
     static func changeCurrentAppBackground() {
-        
+        let defaults = UserDefaults.standard
+        let darkmode = defaults.bool(forKey: "darkMode")
+        defaults.set(!darkmode, forKey: "darkMode")
     }
 }
