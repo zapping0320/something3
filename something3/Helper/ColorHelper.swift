57bc4bb1f3388a022abdfe572e02b11b9516b978
John DongHyun Kim <zapping0320@gmail.com>
2018-11-25 21:08:44 +0900

    theme 적용 범위를 정함

--- something3/Helper/ColorHelper.swift
+++ something3/Helper/ColorHelper.swift
@@ -10,18 +10,52 @@ import Foundation
 import UIKit
 
 class ColorHelper {
-    static let mainBackgroundColor = UIColor(red: 255/255, green: 175/255, blue: 50/255, alpha: 1)
+    //normal
+    static let mainAppBackgroundColor = UIColor(red: 255/255, green: 175/255, blue: 50/255, alpha: 1)
+    static let mainButtonBackgroundColor = UIColor(red: 255/255, green: 142/255, blue: 87/255, alpha: 1)
+    static let mainTextColor = UIColor.black
+    
+    //dark
+    static let darkAppBackgroundColor = UIColor.lightGray
+    static let darkButtonBackgroundColor = UIColor.gray
+    static let darkTextColor = UIColor.white
     
     static func getCurrentAppBackground() -> UIColor {
         let defaults = UserDefaults.standard
         let darkmode = defaults.bool(forKey: "darkMode")
         if(darkmode == true)
         {
-            return UIColor.lightGray
+            return darkAppBackgroundColor
         }
         else
         {
-            return mainBackgroundColor
+            return mainAppBackgroundColor
+        }
+    }
+    
+    static func getCurrentMainButtonColor() -> UIColor {
+        let defaults = UserDefaults.standard
+        let darkmode = defaults.bool(forKey: "darkMode")
+        if(darkmode == true)
+        {
+            return darkTextColor
+        }
+        else
+        {
+            return mainTextColor
+        }
+    }
+    
+    static func getCurrentTextColor() -> UIColor {
+        let defaults = UserDefaults.standard
+        let darkmode = defaults.bool(forKey: "darkMode")
+        if(darkmode == true)
+        {
+            return darkButtonBackgroundColor
+        }
+        else
+        {
+            return mainButtonBackgroundColor
         }
     }
     
