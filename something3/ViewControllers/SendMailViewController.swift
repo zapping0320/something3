4917bfff6ec8d671e38f3c607c1d2c535645786c
John DongHyun Kim <zapping0320@gmail.com>
2018-12-28 07:30:49 +0900

    Setting - send mail에 place holder 추가함

--- something3/ViewControllers/SendMailViewController.swift
+++ something3/ViewControllers/SendMailViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class SendMailViewController: UIViewController {
+class SendMailViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var tf_senderAddress: UITextField!
-    
     @IBOutlet weak var tv_sendContents: UITextView!
+    
+    let contentPlaceHolder:String = "문제를 설명하거나 아이디어를 공유하세요"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.tv_sendContents.delegate = self
+        self.tv_sendContents.text = self.contentPlaceHolder
+        self.tv_sendContents.textColor = UIColor.lightGray
     }
     
-
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = self.contentPlaceHolder
+            textView.textColor = UIColor.lightGray
+        }
+    }
    
 
 }
