962f9a9bc96a8bd0946aa3a6530bc842d58c70a0
John DongHyun Kim <zapping0320@gmail.com>
2018-12-31 07:50:54 +0900

    Setting에 Tag 메뉴 추가함

---
+++ something3/ViewControllers/ManageTagsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ManageTagsViewController.swift
+//  something3
+//
+//  Created by 김동현 on 31/12/2018.
+//  Copyright © 2018 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class ManageTagsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
