19a673b299893c6830cfadd63625a17e72d331ab
John DongHyun Kim <zapping0320@gmail.com>
2019-01-01 08:31:38 +0900

    Setting-Tag관리 - search bar 추가함

--- something3/ViewControllers/ManageTagsViewController.swift
+++ something3/ViewControllers/ManageTagsViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ManageTagsViewController: UIViewController {
+class ManageTagsViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var sb_searchBar: UISearchBar!
+    @IBOutlet weak var tableview: UITableView!
+    let cellIdentifier: String = "tagCell"
+    var searchText_: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.initSearchInfo()
+    }
+    
+    func initSearchInfo() {
+        self.sb_searchBar.text = ""
+        self.searchText_ = ""
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchText_ = searchText
+        //loadNotes()
     }
 
 }
