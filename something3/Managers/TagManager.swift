375897314dac1bd4659c5d115843b7ea53aa9780
John DongHyun Kim <zapping0320@gmail.com>
2019-01-02 07:49:36 +0900

    TagManager class 파일 추가

---
+++ something3/Managers/TagManager.swift
@@ -0,0 +1,23 @@
+//
+//  TagManager.swift
+//  something3
+//
+//  Created by 김동현 on 02/01/2019.
+//  Copyright © 2019 John Kim. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class TagManager {
+    
+    func addTagsToNote(noteid:Int, tagString:String) -> Bool {
+        if tagString == "" {
+            return false
+        }
+        
+        
+        return true
+    }
+    
+}
