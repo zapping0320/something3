0c9e8f31735ca93eb0463f48bf31960aeb9bbb1e
John DongHyun Kim <zapping0320@gmail.com>
2019-01-09 07:41:31 +0900

    노트 검색 / favorite 검색에 사용할 tag 필터 파일 추가함

---
+++ something3/ViewControllers/TagFilterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TagFilterViewController.swift
+//  something3
+//
+//  Created by 김동현 on 09/01/2019.
+//  Copyright © 2019 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class TagFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
