a9c983b36b9d9d062b7135574e1fe7a16fe8be5a
John DongHyun Kim <zapping0320@gmail.com>
2019-01-11 07:33:11 +0900

    TagFilterVC에  Close 버튼 달기

--- something3/ViewControllers/TagFilterViewController.swift
+++ something3/ViewControllers/TagFilterViewController.swift
@@ -10,21 +10,17 @@ import UIKit
 
 class TagFilterViewController: UIViewController {
 
+    @IBOutlet weak var button_CloseVC: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func action_CloseVC(_ sender: Any) {
+        self.closeViewController()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func closeViewController() {
+        self.dismiss(animated: false, completion: nil)
     }
-    */
-
 }
