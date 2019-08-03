5b2b4f84ac3d7ddeaacb95f65e52c9181841b33a
John DongHyun Kim <zapping0320@gmail.com>
2019-08-04 07:32:20 +0900

    fix favorite VC intial bug

--- something3/ViewControllers/FavoriteViewController.swift
+++ something3/ViewControllers/FavoriteViewController.swift
@@ -62,8 +62,6 @@ class FavoriteViewController: UIViewController, UISearchBarDelegate {
         let recentPredicate = NSPredicate(format: "isfavorite = true")
         let recentResults = realm.objects(R_Note.self).filter(recentPredicate).sorted(byKeyPath: "updated_at", ascending: false)
         let itemCount = recentResults.count > 4 ? 3 : recentResults.count - 1
-        let notearray_recent = Array(recentResults[0...itemCount])
-        
         
         if(self.searchText_ == "")
         {
@@ -78,8 +76,13 @@ class FavoriteViewController: UIViewController, UISearchBarDelegate {
             let results = realm.objects(R_Note.self).filter(predicateSearch).sorted(byKeyPath: "updated_at", ascending: false)
             notearray_all = Array(results)
         }
+        if(itemCount > 0){
+            let notearray_recent = Array(recentResults[0...itemCount])
+            favoriteNotes[0] = notearray_recent
+        }else {
+            favoriteNotes[0] = [R_Note]()
+        }
         
-        favoriteNotes[0] = notearray_recent
         favoriteNotes[1] = notearray_all
         
         self.tableview?.reloadData()
